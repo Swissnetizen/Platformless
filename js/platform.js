e59283943168cb4a812f20989cb1075a0b9db097
Samarth AGARWAL <samu.agarwal@gmail.com>
2014-07-26 03:43:22 +0200

    Fix git screw-up

--- js/platform.js
+++ js/platform.js
@@ -1,54 +1,99 @@
 define(["c"], function (require) {
+  //General function to define different platforms
   Crafty.c("PlatN", {
-    PlatN: function (n, active) {
-      this.protruding = true;
+    init: function () {
+      //Define setters
+      Object.defineProperties(this,{
+        "platformNumber": {
+          get: function () {
+            return this._platformNumber;
+          },
+          set: this._platformNumberChanged
+        },
+        "active": {
+          set: this._activeChanged,
+          get: function () {
+            return this._active;
+          },
+        }
+      });
+      //Set active to true.
+      //Default
+      this.platformNumber = 1;
+      this.active = true;
+      //Add components
       this.requires("Color");
       this.addComponent("Solid");
-      this.bind("P" + n + "Change", this._onPNChange.bind(this));
-      //Select Colour palete
+    },
+    PlatN: function (n, active) {
+      this.platformNumber = 1;
+      //Does it start retracted?
+      this.active = active;
+      //It starts extended
+    },
+    _platformNumberChanged: function (n) {
+      //Unbind previous handler, if any.
+      this.unbind("P" + this._platformNumber + "Change");
+      //Set platform number
+      this._platformNumber = n;
+      //Set colourValues
       if (n === 1) {
+        //Blue
         this.colorValues = {
           primary: "#0000FF",
           secondary: "#000099"
         }
       } else if (n === 2) {
+        //Red
         this.colorValues = {
           primary: "#FF0000",
           secondary: "#990000"
         }
       }
-      //Does it start retracted?
-      if (active === false) {
-        this.protruding = active;
-        this.removeComponent("Solid");
-        this.color(this.colorValues.secondary);
-        //It starts extended
-      } else this.color(this.colorValues.primary);
+      //Get activeChanged function to reset colours
+      this._activeChanged(this._active);
+      //Bind event handler
+      this.bind("P" + n + "Change", this._onPNChange.bind(this));
     },
-    _onPNChange: function () {
-      this.protruding = !this.protruding;
-      if (this.protruding) {
+    _activeChanged: function (value) {
+      console.log(value);
+      this._active = value;
+      if (value) {
         this.addComponent("Solid");
         this.color(this.colorValues.primary);
       } else {
-        console.log("HIDE");
         this.removeComponent("Solid");
         this.color(this.colorValues.secondary);
       }
+    },
+    _onPNChange: function () {
+      if (this._platformNumber === 1) {
+        console.log("TRIGGER");
+      } else if (this._platformNumber === 2) {
+        console.log("TRIG JOKES ON YOU")
+      }
+      this.active = !this._active;
     }
   });
   //Shorter functions to init the top one
   //Plat1
   Crafty.c("Plat1", {
-    Plat1: function (active) {
+    init: function () {
       this.addComponent("PlatN");
-      this.PlatN(1, active);
+      this.platformNumber = 1;
+    },
+    Plat1: function (active) {
+      this.active = active;
     }
   });
+//Plat 2
   Crafty.c("Plat2", {
-    Plat2: function () {
+    init: function () {
       this.addComponent("PlatN");
-      this.PlatN(2);
+      this.platformNumber = 2;
+    },
+    Plat2: function (active) {
+      this.active = active;
     }
   });
 });
